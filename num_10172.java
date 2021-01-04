a72f9a1bbde352b22173866928861aca13af98bd
Yujin Jeon <75886211+YujinJeon416@users.noreply.github.com>
2021-01-04 11:02:01 +0900

    아래 예제와 같이 개를 출력하시오.

---
+++ num_10172.java
@@ -0,0 +1,12 @@
+public class num_10172 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"'    |");
+		System.out.println("||_/=\\\\__|");
+	}
+
+}

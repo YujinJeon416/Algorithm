c5870316d2a5e4d578bffae932b5b4e57076598c
Yujin Jeon <75886211+YujinJeon416@users.noreply.github.com>
2021-01-31 20:57:32 +0900

    Update and rename num_10809 to num_10809.java

--- num_10809.java
+++ num_10809.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class num_10809 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
 		String str = sc.next();
 
